ff3e43733101f68c81ee3e73ad964b26e741a62b
Akhila Raju <akhila@berkeley.edu>
2019-07-11 12:52:04 -0400

    test

--- packages/contracts/contracts/TestContract.sol
+++ packages/contracts/contracts/TestContract.sol
@@ -1,31 +1,14 @@
 pragma solidity ^0.5.0;
 
-import { CommitmentChain } from "./CommitmentChain.sol";
-import { Deposit } from "./Deposit.sol";
-
-
 contract EmptyContract {
   address public authenticationAddress;
   address public commitmentChain;
   mapping(address => address) public depositContracts;
-  // mapping (bytes32 => uint256)  exitRedeemableAfter;
-
-  uint public id;
-  // mapping(string => string) public metadata;
-
-  constructor() public {}
 }
 
-contract NewContract {
-  address public authenticationAddress;
-  address public commitmentChain;
+contract TestContract {
   mapping(address => address) public depositContracts;
   mapping (bytes32 => uint256) public exitRedeemableAfter;
-
-  uint public id;
-  // mapping(string => string) public metadata;
-
-  constructor() public {}
 
   function doDeploy() public {
    new EmptyContract();
